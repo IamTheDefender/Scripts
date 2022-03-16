64acad40edf66c878aeb90d3cdf87a78c86a80dc
Kepler <72926871+IamTheDefender@users.noreply.github.com>
2022-03-16 19:34:44 +0530

    Update Miner.sh

--- Miner.sh
+++ Miner.sh
@@ -1,3 +1,3 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
 tar -xf lolMiner_v1.42_Lin64.tar.gz 
-cd 1.42 && ./lolMiner -a ETHASH --pool asia1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.3g8k9t324s4ypz7
+cd 1.42 && ./lolMiner -a ETHASH --pool asia1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.z71gmwvj2tm2m18
